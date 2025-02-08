78d4c3681b8da63c64deb768f855f85813af0d8f
Rafael-1357 <rafaellsilveira1357@gmail.com>
2025-02-08 16:50:58 -0300

    New Layout Folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/home"
-import Products from "./pages/products"
-import Sales from "./pages/sales"
-import Statistics from "./pages/statistics"
+import './App.css'
+import { Button } from "@/components/ui/button"
+
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={Home()} />
-        <Route path="/produtos" element={Products()} />
-        <Route path="/vendas" element={Sales()} />
-        <Route path="/estatisticas" element={Statistics()} />
-        <Route path="*" element={<h1>Not found</h1>}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
   )
 }
 
